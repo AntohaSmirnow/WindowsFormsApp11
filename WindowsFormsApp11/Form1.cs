ff4b63a3248cee68ea0a75d38e4b8ebe0d52300f
Антон Смирнов <t.stolishsmirnow@yandex.ru>
2021-12-24 09:54:25 +0300

    1

--- WindowsFormsApp11/Form1.cs
+++ WindowsFormsApp11/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,47 @@ namespace WindowsFormsApp11
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Файл (*.csv)|*.csv";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(dlg.FileName, false, Encoding.Unicode))
+                {
+                    sw.WriteLine("Текст: " + textBox1.Text);
+                   
+                }
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//StreamWriter i = new StreamWriter(@"C:\Users\acer\source\repos\WindowsFormsApp11\1.csv");
+            //i.WriteLine(textBox1.Text, '\n');
+            //i.Close();
+
+            //using (StreamWriter incdate = File.AppendText(@"C:\Users\acer\source\repos\WindowsFormsApp11\1.csv"))
+            //{
+            //    incdate.WriteLine(textBox1.Text, '\n');
+            //}
